5642d3ebcd957a010015fd591b52baac61af303f
zeninnikita22 <zeninnikita@gmail.com>
2022-03-26 12:18:10 +0200

    made my JS clock more interactive, now it is receiving current date, hour, minute and second

--- Task2-JavaScript-Clock/script.js
+++ Task2-JavaScript-Clock/script.js
@@ -2,20 +2,22 @@ const secondArrow = document.querySelector(".second");
 const minuteArrow = document.querySelector(".minute");
 const hourArrow = document.querySelector(".hour");
 
-let numberOfDegreesSecond = 0;
+let currentDate = new Date();
+
+let numberOfDegreesSecond = currentDate.getSeconds() * 6;
 window.onload = setInterval(function () {
   numberOfDegreesSecond = numberOfDegreesSecond + 6;
   secondArrow.style.transform = `rotate(${numberOfDegreesSecond}deg)`;
 }, 1000);
 
-let numberOfDegreesMinute = 0;
+let numberOfDegreesMinute = currentDate.getMinutes() * 6;
 window.onload = setInterval(function () {
   numberOfDegreesMinute = numberOfDegreesMinute + 6;
   minuteArrow.style.transform = `rotate(${numberOfDegreesMinute}deg)`;
 }, 60000);
 
-let numberOfDegreesHour = 0;
+let numberOfDegreesHour = currentDate.getHours() * 30;
 window.onload = setInterval(function () {
-  numberOfDegreesHour = numberOfDegreesHour + 6;
+  numberOfDegreesHour = numberOfDegreesHour + 30;
   hourArrow.style.transform = `rotate(${numberOfDegreesHour}deg)`;
 }, 3600000);
